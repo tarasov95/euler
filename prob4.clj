0221cb2f34ee15ee21a0170d17180ebc5fee4ac3
Sergey Tarasov <lysy95@gmail.com>
2018-08-02 01:05:24 +0800

    prob4, done

---
+++ prob4.clj
@@ -0,0 +1,21 @@
+(ns prob4)
+
+(defn is-palin
+  ([n m]
+   (let [d (mod n 10)]
+     (cond (= n m) true
+           (and (= m 0) (= 0 d)) false
+           (> m n) false
+           :else (recur (/ (- n d) 10) (+ (* 10 m) d)))))
+  ([n] (is-palin n 0)))
+
+(defn gen-prod [c]
+  (let [up (long (Math/pow 10 c))
+        rg (range (/ up 10) up)]
+    (for [x rg y rg
+          :when (<= y x)]
+      (* x y))))
+
+(map
+ #(reduce max (filter is-palin (gen-prod %)))
+ [2 3])
