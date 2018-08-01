abcd367a0a87fdcfc6ff4fc2c8584f61f312b1f3
Sergey Tarasov <lysy95@gmail.com>
2018-08-01 10:18:15 +0800

    prob3, wip

--- prob3.clj
+++ prob3.clj
@@ -5,8 +5,10 @@
 (defn not-divider-of [x]
   (comp (partial not= 0) (partial mod x)))
 
-(defn prime? [x rg]
-  (every? (not-divider-of x) rg))
+(defn prime?
+  "x:value to test; rg:array of primes to test against"
+  [x rg]
+  (every? (not-divider-of x) rg)) ;TODO: optimize to check only up to (Math/sqrt)
 
 (defn conj-prime [r x]
   (if (prime? x r)
@@ -16,5 +18,18 @@
 (defn list-primes [N]
   (reduce conj-prime [2 3 5 7 11] (filter (comp not even?) (range 13 N))))
 
-(list-primes (Math/sqrt N))
+(defn prime-pow "Number poWer Prime" [n w p]
+  (if (or (<= n 1) (not= 0 (mod n p)))
+    [n w p]
+    (prime-pow (/ n p) (inc w) p)))
 
+(defn conj-prime-pow "Result Element" [r e]
+  (let [[n _ _] (last r)]
+    (if (<= n 1)
+      (reduced r)
+      (conj r (prime-pow n 0 e)))))
+
+(reduce
+ conj-prime-pow
+ [(prime-pow N 0 2)]
+ (filter #(> % 2) (list-primes N)))
