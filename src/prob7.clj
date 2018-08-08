32f3eb18aeff01ff5de25e7bc71b325c3b0588f0
Sergey Tarasov <lysy95@gmail.com>
2018-08-08 09:28:31 +0800

    prob7, code cleanup

--- src/prob7.clj
+++ src/prob7.clj
@@ -2,26 +2,11 @@
   (:require
    [taoensso.tufte :as tufte :refer (defnp p profiled profile)]))
 
-;; (defn prime? [rg n]
-;;   (let [b (p :sqrt-in-prime? (Math/sqrt n))]
-;;     (p :not-any-in-prime? (not-any? #(p :mod-test-in-prime? (zero? (mod n %))) (p :take-while-in-prime? (take-while (partial < b) rg))))))
-
-;; (defn find-next-prime [l rg]
-;;   (loop [p (+ 2 l)]
-;;     (if (tufte/p :prime?-in-find (prime? p rg))
-;;       p
-;;       (recur (+ 2 p)))))
-
 (defn prime? [rg n]
   (not (some #(zero? (mod n %)) rg)))
 
 (defn find-next-prime [n rg]
   (first (filter (fn [e] (prime? (take-while #(<= (* % %) e) rg) e)) (iterate inc (inc n)))))
-
-;; (take-while #(<= (* % %) 4) '(2 3 5))
-;; (prime? '(2) 4)
-(find-next-prime 3 '(2 3 5))
-(find-next-prime 2 '(2))
 
 (defn prime-seq
   ([] (prime-seq [2] 2 1))
@@ -29,22 +14,19 @@
    (lazy-seq
     (let [n (find-next-prime l rg)]
       ;; (println rg)
-      (when (zero? (mod ix 100)) (println "l= " l " realized #" ix "=" n))
+      ;; (when (zero? (mod ix 100)) (println "l= " l " realized #" ix "=" n))
       (p :cons1 (cons n (prime-seq (p :conj2 (conj rg n)) n (inc ix))))))))
 
 (tufte/add-basic-println-handler! {})
-(profile {} (last (take 10001 (prime-seq))))
+(profile {} (last (take 10000 (prime-seq))))
 
+;; https://clojuredocs.org/clojure.core/lazy-seq
 ;; (defn sieve [s]
 ;;   (cons (first s)
 ;;         (lazy-seq (sieve (filter #(not= 0 (mod % (first s)))
 ;;                                  (rest s))))))
 
-
-;; (last (take 10000 (sieve (iterate inc 2))))
-
-
-;; (def prime-numbers
+;; https://www.google.com/url?q=https%3A%2F%2Fgist.github.com%2Fkohyama%2F8e599b2e765ad4256f32&sa=D&sntz=1&usg=AFQjCNEFpYFTyEo6PbVtAkCWSmE8k8muvw;; (def prime-numbers
 ;;   ((fn f [x]
 ;;      (cons x
 ;;            (lazy-seq
@@ -55,8 +37,3 @@
 ;;                          (take-while #(<= (* % %) n) prime-numbers)))
 ;;                  (iterate inc (inc x))))))))
 ;;    2))
-
-;; (last (take 10000 prime-numbers))
-
-;; {:l (cons 2 '(1))
-;;  :a (cons 2 [1])}
