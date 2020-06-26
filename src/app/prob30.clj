51acef99b52d9212f5f1179c0fda0a8cbd81e0e0
tarasov95 <lysy95@gmail.com>
2020-06-26 09:06:36 +0800

    prob30: fix, r/fold

--- src/app/prob30.clj
+++ src/app/prob30.clj
@@ -1,4 +1,4 @@
-(ns app.prob29
+(ns app.prob30
   (:require [lib.numb :as n]
             [clojure.core.reducers :as r]))
 
@@ -12,11 +12,11 @@
        (r/reduce + )))
 
 (defn max-range [N]
-  (sum-pw (repeat N 9) N))
+  (sum-pw (repeat (inc N) 9) N)) ;; 10^4 ~> 5 digits, hence the inc
 
 (defn list-num [P]
   (let [U (inc (max-range P))]
     (r/filter (fn [e] (= e (sum-pw (n/num2dig 10 e) P)))
-              (range 9 U))))
+              (vec (range 9 U))))) ;;vec is to support r/fold
 
-(reduce + (list-num 5))
+(time (r/fold + (list-num 5)))
