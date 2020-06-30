8d260e0b94c78998b215fb90d7f586662b444cae
tarasov95 <lysy95@gmail.com>
2020-06-30 17:17:34 +0800

    prob31/reducers,pmap

--- src/app/prob31.clj
+++ src/app/prob31.clj
@@ -8,7 +8,7 @@
 
 (defn try-one-coin [amount c]
   (->> (range-of-coin-* amount c)
-       (map (fn [m] {:$ c :* m :v (* c m)}))))
+       (r/map (fn [m] {:$ c :* m :v (* c m)}))))
 
 (defn change
   ([amount coins] (change [] [] amount coins))
@@ -17,8 +17,26 @@
      (if (= 0 amount) (conj z y) z)
      (let [c (first coins)]
        (->> (try-one-coin amount c)
-            (mapcat (fn [e] (change z (conj y e) (- amount (:v e)) (rest coins))))
-            (filter not-empty))))))
+            (r/map (fn [e] (change z (conj y e) (- amount (:v e)) (rest coins))))
+            (r/filter not-empty)
+            (r/fold concat))))))
 
-(count (change 100 [1 5 10 25 50]))
+;; change returns all solutions for it's parameters as an array of arrays of $* structures
+;; when adding a coin to the solution of n-1 complexity have to join all $* for that coin with all of the soluions in the above array
 
+;; (count (change 100 [1 5 10 25 50]))
+;; 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
+;; 
+
+(defn pchange [amount coins]
+  (let [c1 (first coins)]
+    (->> (into (list) (try-one-coin amount c1))
+         (pmap (fn [e] (change [] [e] (- amount (:v e)) (rest coins))))
+         (filter not-empty)
+         (reduce concat))))
+
+(let [coins  [1 2 5 10 20 50 100 200]
+      amount 200]
+  (time (pmap identity
+              [(time (count (change amount coins)))
+               (time (count (pchange amount coins)))])))
