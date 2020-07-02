ebe4a6cf148e9d3ea2923f332c6f9b6938a5eb54
tarasov95 <lysy95@gmail.com>
2020-07-02 10:18:06 +0800

    prob32/no-deps

--- src/app/prob32.clj
+++ src/app/prob32.clj
@@ -1,20 +1,9 @@
-(ns app.prob32
-  (:require [clojure.core.reducers :as r]
-            [lib.numb :as numb]))
+(ns app.prob32)
 
 ;; https://projecteuler.net/problem=32
 ;; The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
 ;; Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
 ;; HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
-
-(def code0 (int \0))
-
-(defn num-to-set [num]
-  (->> (seq (str num))
-       (map (fn [ch] (- (int ch) code0)))
-       (into #{})))
-
-(def all-dig (num-to-set 123456789))
 
 ;; 123456789 (count (seq "123456789")) ~> 9 digits
 ;; we have to have 9 digits in the  "identity"
@@ -24,14 +13,14 @@
 ;; (max of the left) ~> 98901 and (max of the right) ~> 9999 ~> (range 1000 10000)
 ;; x*y=z ~> z>=x && z>=y ~> the right hand size has to be 4 digits
 
-(defn list-num-distinct-dig [rg]
-  (->> rg
-       (map (fn [n] {:num n :dig (num-to-set n)}))
-       (filter (fn [nd] (and (not (get (:dig nd) 0))
-                             (= (count (:dig nd)) (count (str (:num nd)))))))))
+(defn is-pandig [N s]
+  (let [d (into #{} (seq s))]
+    (and (not (d \0))
+         (= N (count s) (count d)))))
 
 (defn list-products []
-  (list-num-distinct-dig (range 1000 10000)))
+  (->> (range 1000 10000)
+       (filter #(is-pandig 4 (str %)))))
 
 (defn list-all-mult-of [nu]
   (for [dv     (range 2 nu)
@@ -40,20 +29,14 @@
         :when  (= 0 (mod nu dv))]
     [dv qu nu]))
 
-(defn is-pandig [N s]
-  (let [d (into #{} (seq s))]
-    (and (not (d \0))
-         (= N (count s) (count d)))))
-
 (defn list-panding-mult-of [nu]
   (->> (list-all-mult-of nu)
        (filter #(is-pandig 9 (apply str %)))))
 
 (defn list-all-pandig9-mult []
   (->> (list-products)
-      (map :num)
-      (mapcat list-panding-mult-of)
-      (filter not-empty)))
+       (mapcat list-panding-mult-of)
+       (filter not-empty)))
 
 (defn solve []
   (->> (list-all-pandig9-mult)
