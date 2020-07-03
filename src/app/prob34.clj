0d643fe354fef17ee0c64ebfd753edf6baf0f8ec
tarasov95 <lysy95@gmail.com>
2020-07-03 18:33:17 +0800

    prob34/macro

--- src/app/prob34.clj
+++ src/app/prob34.clj
@@ -7,31 +7,48 @@
 ;; 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
 ;; Find the sum of all numbers which are equal to the sum of the factorial of their digits.
 
-;; 1*1 + 1*2*3*4 + 1*2*3*4*5 = 1 + 1*2*3*4*(1 + 5) => 145
-
-(def fact (memoize numb/fact))
-
-(defn sum-fact [n]
-  (->> (-> n str seq)
-       (r/map #(- (int %) (int \0)))
-       (r/map fact)
-       (r/reduce +)))
-
-;; (->>
-;;  (range 0 10)
-;;  (map #(vector % (fact %))))
-
 ;; => ([0 1] [1 1] [2 2] [3 6] [4 24] [5 120] [6 720] [7 5040] [8 40320] [9 362880])
 ;; if n contains 5 ~> n >= 120
 ;; if n contains 7 ~> n >= 5040
 ;; etc.
 ;; if n contains 9 ~> n >= 362880
 
+
+;; (def mem-fact (memoize numb/fact))
+
+(def map-fact
+  ;; (into {} (map #(vector % (numb/fact %)) (range 0 10) ))
+  (into [] (map numb/fact (range 0 10) )))
+
+(defmacro map-to-case [e]
+  (let [clauses (mapcat #(vector (char (+ % (int \0)))
+                                 (numb/fact %))
+                        (range 0 10))]
+    (concat (list `case e) clauses)))
+
+(defn case-fact [n]
+  (map-to-case n))
+
+(defn sum-fact [n]
+  (->> (-> n str seq)
+       ;; (r/map #(- (int %) (int \0)))
+       (r/map case-fact)
+       (r/reduce +)))
+
 (defn get-max-range [n]
   (if (> n (sum-fact n))
     n
     (recur (+ 9 (* n 10)))))
 
-(time
- (->> (range 10 (get-max-range 9))
-      (filter #(= % (sum-fact %)))))
+(defn candidates []
+  (range 10 (sum-fact (get-max-range 9))))
+
+(defn solve []
+  (->> (vec (candidates))
+       (r/filter #(= % (sum-fact %)))
+       (r/fold +)))
+
+(time (solve))
+
+
+
