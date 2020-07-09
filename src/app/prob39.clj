f39819adf7a9b1ef05c521a51cf4c164921d20e2
tarasov95 <lysy95@gmail.com>
2020-07-09 09:47:03 +0800

    prob39/cleanup

--- src/app/prob39.clj
+++ src/app/prob39.clj
@@ -25,63 +25,10 @@
         c (* k (+ m2 n2))]
     (with-meta [(min a b) (max a b) c] {:nmk [n m k]})))
 
-(defn gen-triples []
-  (for [n (range 1 5)
-        m (range (inc n) 10)
-        k (range 1 10)
-        :let [m2 (* m m)
-              n2 (* n n)
-              a (* k (- m2 n2))
-              b (* k (* 2 m n))
-              c (* k (+ m2 n2))]]
-    (with-meta [(min a b) (max a b) c] {:nmk [n m k]})))
-
-(defn test1 []
-  (binding [*print-meta* true]
-    (pr (->> (gen-triples)
-            ;; (map (partial apply perim))
-            ;; (into #{})
-             (group-by (partial apply perim))
-            ;; (map #(vector (first %) (count (second %))))
-             (filter #(= 120 (first %)))))))
-
-(defn check []
-  (filter #(not (apply triple? %)) (gen-triples)))
-
-(defn gen-loop [z param next-param]
-  (let [[n m k] param]
-    (let [t (triple n m k)
-          p (perim t)]
-      (if (<= p 1000)
-        (recur (conj z t) (next-param param) next-param)
-        z))))
-
-(defmacro inc-ix [ix]
-  `(fn [v#] (assoc v#  ~ix (inc (v# ~ix)))))
-
-(def inc-n (inc-ix 0))
-(def inc-m (inc-ix 1))
-(def inc-k (inc-ix 2))
-
-(defn explore1 []
-  (into #{} (concat
-             (->> (range 1 100)
-                  (map #(triple % (inc %) 1))
-                  (take-while #(<= (apply perim %) 1000)))
-             (->> (range 1 100)
-                  (map #(triple 1 (inc %) 1))
-                  (take-while #(<= (apply perim %) 1000))))))
-
-
-;; (gen-loop [] [1 2 1] inc-k)
-;; (macroexpand '(inc-ix 2))
-
-;; if(p>=1000 and k=1) inc m
-;; if(p>=1000 and m=n+1) inc n
-;; 
-
-(defn gen2
-  ([P] (gen2 P [] 1 2 1))
+;;TODO: try to get rid of the duplicates by checking for m-n oddity and them being coprime (i.e. gcd = 1)
+(defn gen-triples
+  ([P] (gen-triples P []))
+  ([P z] (gen-triples P z 1 2 1))
   ([P z n m k]
    (let [t (triple n m k)
          p (perim t)]
@@ -93,14 +40,12 @@
          (> m (+ n 1)) (recur P z (inc n) (+ 2 n) 1)
          :else z)))))
 
-;; (time (binding [*print-meta* true]
-;;    (pr (->> (gen2 1000)
-;;             (into #{})
-;;             (filter #(= 120 (perim %)))))))
+(defn solve []
+  (->> (gen-triples 1000 #{})
+       (group-by perim)
+       (map #(vector (first %) (count (second %))))
+       ;; (sort-by second)
+       ;; (take-last 1)
+       (reduce (partial max-key second))))
 
-(->> (gen2 1000)
-     (into #{})
-     (group-by perim)
-     (map #(vector (first %) (count (second %))))
-     (sort-by second)
-     (take-last 1))
+(time (solve))
