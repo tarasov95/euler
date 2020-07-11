0f63ca21403462a57733255b6f3b9d4ebc09e069
tarasov95 <lysy95@gmail.com>
2020-07-12 00:35:19 +0800

    prob43

--- src/app/prob43.clj
+++ src/app/prob43.clj
@@ -1,7 +1,7 @@
 (ns app.prob43
   (:require
    [lib.numb :as numb]
-   [clojure.string :as s]))
+   [lib.prime :as prime]))
 
 ;; https://projecteuler.net/problem=43
 
@@ -39,20 +39,24 @@
       (and (fd er)
            (= 0 (mod (+ er dd) div))))))
 
-(defn solve []
+(defn solve-draft []
   (let [d1  (range 1 10)
         p2  (join filter-dig concat-d3 d1 (part-d3 2))
         p3  (join (partial filter-dig&div 3) concat-d1 p2 (part-d1))
         p5  (join (partial filter-dig&div 5) concat-d1 p3 (part-d1))
         p7  (join (partial filter-dig&div 7) concat-d1 p5 (part-d1))
         p11 (join (partial filter-dig&div 11) concat-d1 p7 (part-d1))
-        p13 (join (partial filter-dig&div 13) concat-d1 p11 (part-d1))]
-    (join (partial filter-dig&div 17) concat-d1 p13 (part-d1))))
+        p13 (join (partial filter-dig&div 13) concat-d1 p11 (part-d1))
+        p17 (join (partial filter-dig&div 17) concat-d1 p13 (part-d1))]
+    p17))
+
+(defn solve []
+  (let [d1  (range 1 10)
+        p2  (join filter-dig concat-d3 d1 (part-d3 2))]
+    (->>
+     (drop 1 (prime/primes-below 18))
+     (reduce (fn [z p]
+               (join (partial filter-dig&div p) concat-d1 z (part-d1)))
+             p2))))
 
 (reduce + (solve))
-;; (1406357289 1430952867 1460357289 4106357289 4130952867 4160357289)
-
-;; (numb/num2dig 2 (numb/dig-mask 1024))
-;; (numb/any-dig-in? (numb/dig-mask 1024) 3)
-;; #(vector %1 %2)
-;; (inner-join [1 2 ] [3 4])
