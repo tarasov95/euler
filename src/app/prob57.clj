75138fbe6b9884ac7c132170396f38781472129b
tarasov95 <lysy95@gmail.com>
2020-07-28 23:32:05 +0800

    prob57

--- src/app/prob57.clj
+++ src/app/prob57.clj
@@ -5,12 +5,28 @@
 
 ;; https://projecteuler.net/problem=57
 
-(->>
- (iterate #(+ 1 (/ 1 (inc %)))
-          (+ 1 (/ 1 2)))
- (map #(vector (numerator %) (denominator %)))
- (map #(map (partial numb/num2dig 10) %))
- (map #(map count %))
- (take 1000)
- (filter #(> (first %) (second %)))
- (count))
+(defn src []
+  (iterate #(+ 1 (/ 1 (inc %)))
+           (+ 1 (/ 1 2))))
+
+(defn solve-lazy []
+  (->> (src)
+       (map #(vector (numerator %) (denominator %)))
+       (map #(map (partial numb/num2dig 10) %))
+       (map #(map count %))
+       (take 1000)
+       (filter #(> (first %) (second %)))
+       (count)))
+
+(defn solve-eager []
+  (transduce (comp
+              (map #(vector (numerator %) (denominator %)))
+              (map #(map (partial numb/num2dig 10) %))
+              (map #(map count %))
+              (map #(if (> (first %) (second %)) 1 0))
+              (take 1000))
+             +
+             (src)))
+
+(time (println "solve-lazy" (solve-lazy)))
+(time (println "solve-eager" (solve-eager)))
