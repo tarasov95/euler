1d524655df8840370bf11a8d5feb25d01b211ec4
tarasov95 <lysy95@gmail.com>
2020-08-01 01:08:18 +0800

    prob59/doc

--- src/app/prob59.clj
+++ src/app/prob59.clj
@@ -7,33 +7,32 @@
 ;; https://idafchev.github.io/crypto/2017/04/13/crypto_part1.html
 
 (def letter-freq-base
-  {
-  \a 	8.497
-  \b 	1.492
-  \c 	2.202
-  \d 	4.253
-  \e 	11.162
-  \f 	2.228
-  \g 	2.015
-  \h 	6.094
-  \i 	7.546
-  \j 	0.153
-  \k 	1.292
-  \l 	4.025
-  \m 	2.406
-  \n 	6.749
-  \o 	7.507
-  \p 	1.929
-  \q 	0.095
-  \r 	7.587
-  \s 	6.327
-  \t 	9.356
-  \u 	2.758
-  \v 	0.978
-  \w 	2.560
-  \x 	0.150
-  \y 	1.994
-  \z 	0.077 })
+  {\a 	8.497
+   \b 	1.492
+   \c 	2.202
+   \d 	4.253
+   \e 	11.162
+   \f 	2.228
+   \g 	2.015
+   \h 	6.094
+   \i 	7.546
+   \j 	0.153
+   \k 	1.292
+   \l 	4.025
+   \m 	2.406
+   \n 	6.749
+   \o 	7.507
+   \p 	1.929
+   \q 	0.095
+   \r 	7.587
+   \s 	6.327
+   \t 	9.356
+   \u 	2.758
+   \v 	0.978
+   \w 	2.560
+   \x 	0.150
+   \y 	1.994
+   \z 	0.077})
 
 (defn letter-freq []
   (->> letter-freq-base
@@ -57,11 +56,15 @@
          (map (partial map rg))
          (map (partial into [])))))
 
-(defn ioc [freqs text-len]
+(defn ioc
+  "index of coincidence as n(n-1)/k(k-1) n(j) is the frequency of letter j in the text and k is the length of the text"
+  [freqs text-len]
   (let [denom (* text-len (dec text-len))]
-     (reduce + (map #(/ (* % (dec %)) denom) freqs))))
+    (reduce + (map #(/ (* % (dec %)) denom) freqs))))
 
-(defn ioc-for-keylen [data key-len]
+(defn ioc-for-keylen
+  "calculates average ioc for all L 'columns' of text for the guessed key length L"
+  [data key-len]
   (let [col (split-msg data key-len)]
     (let [fq (->> col
                   (map frequencies)
@@ -72,17 +75,19 @@
 
 (defn guess-ley-len [ciph]
   (let [iocs (->> (range 1 1000)
-                 (map (partial ioc-for-keylen ciph)))]
-   (->> (map-indexed vector iocs)
-        (filter #(> (second %) 0.06))
-        (first)
-        (first)
-        (inc))))
+                  (map (partial ioc-for-keylen ciph)))]
+    (->> (map-indexed vector iocs)
+         (filter #(> (second %) 0.06)) ;;0.063 is the theoretical "unnormalized" IOC for English
+         (first)
+         (first)
+         (inc))))
 
 (defn xor-col [col x]
   (map (partial bit-xor x) col))
 
-(defn eval-col-freqs [lfq col]
+(defn eval-col-freqs
+  "statistical distance 'sum(f*n)' between the theoretical letter frequencies lfq and actual freqs in a single column col"
+  [lfq col]
   (->> (frequencies col)
        (map #(* (or (lfq (first %)) 0) (second %)))
        (reduce +)))
@@ -97,21 +102,20 @@
 
 (defn guess-key [ciph]
   (let [key-len (guess-ley-len ciph)
-       cols (split-msg ciph key-len)]
-   (->> (map guess-xor-for-col cols)
-        (map #(+ (int \a) (first %)))
-        (into []))))
+        cols (split-msg ciph key-len)]
+    (->> (map guess-xor-for-col cols)
+         (map #(+ (int \a) (first %)))
+         (into []))))
 
 (defn apply-ckey
-  ""
+  "apply key to text via XOR"
   [ckey ix ch]
   (bit-xor ch (ckey (mod ix (count ckey)))))
 
 (defn decrypt [ciph]
   (let [ckey (guess-key ciph)]
-   (->> ciph
-        (map-indexed (partial apply-ckey ckey))
-        )))
+    (->> ciph
+         (map-indexed (partial apply-ckey ckey)))))
 
 (defn solve []
   (->> (decrypt (data))
