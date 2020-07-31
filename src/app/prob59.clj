2eb2b61ea062e787c18ef1a6a5209863fc7a5e13
tarasov95 <lysy95@gmail.com>
2020-08-01 00:58:24 +0800

    prob59

--- src/app/prob59.clj
+++ src/app/prob59.clj
@@ -2,7 +2,43 @@
   (:require [clojure.string :as s]))
 
 ;; https://projecteuler.net/problem=59
+;; https://crypto.stackexchange.com/questions/333/how-does-the-index-of-coincidence-work-in-the-kasiki-test
+;; https://en.wikipedia.org/wiki/Index_of_coincidence
+;; https://idafchev.github.io/crypto/2017/04/13/crypto_part1.html
 
+(def letter-freq-base
+  {
+  \a 	8.497
+  \b 	1.492
+  \c 	2.202
+  \d 	4.253
+  \e 	11.162
+  \f 	2.228
+  \g 	2.015
+  \h 	6.094
+  \i 	7.546
+  \j 	0.153
+  \k 	1.292
+  \l 	4.025
+  \m 	2.406
+  \n 	6.749
+  \o 	7.507
+  \p 	1.929
+  \q 	0.095
+  \r 	7.587
+  \s 	6.327
+  \t 	9.356
+  \u 	2.758
+  \v 	0.978
+  \w 	2.560
+  \x 	0.150
+  \y 	1.994
+  \z 	0.077 })
+
+(defn letter-freq []
+  (->> letter-freq-base
+       (map #(vector (int (first %))  (second %)))
+       (into {})))
 
 (defn load-data []
   (let [data (slurp "resources/p059_cipher.txt")
@@ -43,5 +79,48 @@
         (first)
         (inc))))
 
+(defn xor-col [col x]
+  (map (partial bit-xor x) col))
 
-(guess-ley-len (data))
+(defn eval-col-freqs [lfq col]
+  (->> (frequencies col)
+       (map #(* (or (lfq (first %)) 0) (second %)))
+       (reduce +)))
+
+(defn guess-xor-for-col [col]
+  (let [lfq (letter-freq)]
+    (->> (range (int \a) (inc (int \z)))
+         (map (partial xor-col col))
+         (map (partial eval-col-freqs lfq))
+         (map-indexed vector)
+         (reduce (partial max-key second)))))
+
+(defn guess-key [ciph]
+  (let [key-len (guess-ley-len ciph)
+       cols (split-msg ciph key-len)]
+   (->> (map guess-xor-for-col cols)
+        (map #(+ (int \a) (first %)))
+        (into []))))
+
+(defn apply-ckey
+  ""
+  [ckey ix ch]
+  (bit-xor ch (ckey (mod ix (count ckey)))))
+
+(defn decrypt [ciph]
+  (let [ckey (guess-key ciph)]
+   (->> ciph
+        (map-indexed (partial apply-ckey ckey))
+        )))
+
+(defn solve []
+  (->> (decrypt (data))
+       (reduce +)))
+
+(defn reveal []
+  (->> (decrypt (data))
+       (map char)
+       (reduce str)))
+
+(time (solve))
+
