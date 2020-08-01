1475e10215031e49d0b0636063b011229fd258b9
tarasov95 <lysy95@gmail.com>
2020-08-02 00:43:31 +0800

    prob60/wip

--- src/app/prob60.clj
+++ src/app/prob60.clj
@@ -1,14 +1,48 @@
 (ns app.prob60
-  (:require [lib.prime :as prime]
+  (:require [lib.prime-data :as data]
             [lib.numb :as numb]))
 
 ;; https://projecteuler.net/problem=60
 ;; The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating them in any order the result will always be prime. For example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
 ;; Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
 
+;; https://projecteuler.net/overview=007
+;; 1 is not a prime.All primes except 2 are odd.All primes greater than 3 can be written in the form  6k+/-1.Any number n can have only one primefactor greater than sqrt(n).The consequence for primality testing of a number n is: if we cannot find a numberf less than or equal sqrt(n) that divides n then n is prime: the only primefactor of n is n itself
+
+(def pw10 (memoize (partial numb/pow-int 10)))
+(def lg10 (memoize (partial numb/log-int 10)))
+
+(defn prime? [prev n]
+  (let [q (long (Math/sqrt n))]
+    ;; (println n q (take-while (partial >= q) prev))
+    (->> (take-while (partial >= q) prev)
+         (every? #(not= 0 (mod n %))))))
+
+(defn find-next-prime [prev]
+  ;; (when (not (counted? prev))
+  ;;   (throw (new Exception "prev must be counted")))
+  (let [cnt (count prev)
+        last-prime (prev (dec cnt))]
+    (when (= 0 (mod cnt 10000))
+      (println cnt last-prime))
+    (loop [step 2
+           n (+ step last-prime)]
+      (when (> (- n last-prime) 1000) (throw (new Exception (str "find-next-prime stuck at " n " started from " last-prime))))
+      (if (prime? prev n)
+        (do
+          ;; (println "found-prime:" n)
+          (cons n (lazy-seq (find-next-prime (conj prev n)))))
+        (recur step (+ step n))))))
+
+;; (def prime-seed [2 3 5 7 11])
+
+(def ^:private prime-inf (concat data/prime-seed (find-next-prime data/prime-seed)))
+
+(def check-prime (memoize (partial prime? prime-inf)))
+
 (defn cat-left [x y]
   (+ y
-     (* x (numb/pow-int 10 (inc (numb/log-int 10 y))))))
+     (* x (pw10 (inc (lg10 y))))))
 
 (defn cat-right [x y]
   (cat-left y x))
@@ -16,12 +50,22 @@
 (defn in-family? [rg p]
   (->> rg
        (mapcat #(vector (cat-left % p) (cat-right % p)))
-       (every? prime/is-prime?)))
+       (every? check-prime)))
 
-(let [pr [3 7 109 673]
-      ps (drop-while
-          (partial >= (reduce max pr))
-          (prime/primes-below 10000000))]
-  (->> ps
-       (filter (partial in-family? pr))
-       (take 1)))
+(defn solve []
+  (let [pr [3 7 109 673]
+        ps (drop-while
+            (partial >= (reduce max pr))
+            prime-inf)]
+    (->> ps
+         (filter (partial in-family? pr))
+         (take 1))))
+
+;; (solve)
+
+;; (find-next-prime prime-seed (last prime-seed))
+;; (prime? prime-inf 15)
+;; (->> (map #(vector (= %1 %2) %1 %2)
+;;       (take 1200 prime-inf)
+;;       (take 1200 data/prime-seed))
+;;      (filter #(not (first %))))
