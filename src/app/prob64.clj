e24a018d8d50bd987c138cf828dd1327ace981f2
tarasov95 <lysy95@gmail.com>
2020-08-11 20:20:35 +0800

    prob64

--- src/app/prob64.clj
+++ src/app/prob64.clj
@@ -6,24 +6,31 @@
 
 ;;https://en.wikipedia.org/wiki/Periodic_continued_fraction
 (defn next-a [p]
-  (let [[a m d S a0 per ix-2a0] p
+  (let [[a m d S a0 len ix] p
         mn (- (* d a) m)
         dn (/ (- S (* mn mn)) d)
-        an (long (Math/floor (/  (+ a0 mn) dn)))
-        ixn (if (= (* 2 a0) an) (inc ix-2a0) ix-2a0)
-        pern (if (= 1 ixn) (inc per) per)]
-    [an mn dn S a0 pern ixn]))
+        an (long (Math/floor (/ (+ a0 mn) dn)))
+        ixn (cond (< ix 0) (inc (- ix))
+                  (= (* 2 a0) an) (- ix)
+                  :else ix)
+        lenn (if (or (= -1 ixn) (= 2 ixn)) (inc len) len)]
+    [an mn dn S a0 lenn ixn]))
+
+(defn intl-seq [S]
+  (->> (iterate next-a [(numb/lsqrt S) 0 1 S (numb/lsqrt S) 0 1])
+       (take-while (fn [r] (<= (last r) 2)))))
 
 (defn a-seq [S]
-  (->> (iterate next-a [(numb/lsqrt S) 0 1 S (numb/lsqrt S) 0 0])
-       (take-while (fn [r] (<= (last r) 2)))
-       (map #(last (butlast %)))
-       (take-last 1)))
+  (map first (intl-seq S)))
+
+(defn period [S]
+  (last (butlast (last (intl-seq S)))))
 
 (defn list-fractions [N]
   (->> (range 2 (inc N))
-      (filter #(-> %  Math/sqrt numb/natural? not))
-      (mapcat a-seq)
-      (filter odd?)))
+       (filter #(-> %  Math/sqrt numb/natural? not))
+       (map period)
+       (filter odd?)))
 
-(count (list-fractions 10000))
+(time (count (list-fractions 10000)))
+
