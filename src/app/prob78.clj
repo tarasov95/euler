5227c5e443cb5bb83c086e6edd4372d631636d05
tarasov95 <lysy95@gmail.com>
2020-09-08 19:59:13 +0800

    prob78

--- src/app/prob78.clj
+++ src/app/prob78.clj
@@ -27,5 +27,37 @@
 
 (t/deftest p-test
   (t/is (->> [1, 1, 2, 3, 5, 7, 11, 15, 22, 30, 42, 56, 77, 101, 135, 176, 231, 297, 385, 490]
-        (map-indexed (fn [ix e] (= (p ix) e)))
-        (every? true?))))
+             (map-indexed (fn [ix e] (= (p ix) e)))
+             (every? true?))))
+
+(defn p-reduce
+  "(fun [z n p(n)])"
+  ([fun] (p-reduce fun nil))
+  ([fun val]
+   (letfn [(sign [k v] (if (even? k) (- v) v))]
+     (loop [x 1N
+            rg [1N]
+            z (fun val 0 (rg 0))]
+       (if (reduced? z)
+         (deref z)
+         (let [rgn (->>  (range (- (K x)) (inc (K x)))
+                         (filter (comp not zero?))
+                         (map (fn [k]
+                                (let [a (- x (/ (* k (dec (* 3 k))) 2))]
+                                  (sign k (if (< a 0) 0 (rg a))))))
+                         (reduce +)
+                         (conj rg))]
+           (recur (inc x) rgn (fun z x (rgn x)))))))))
+
+
+(t/deftest p-reduce-test
+  (letfn [(p_ [N] (p-reduce (fn [z n pn] (when (>= n N) (reduced pn)))))]
+    (t/is (->> [1, 1, 2, 3, 5, 7, 11, 15, 22, 30, 42, 56, 77, 101, 135, 176, 231, 297, 385, 490]
+              (map-indexed (fn [ix e] (= (p_ ix) e)))
+              (every? true?)))))
+
+
+(time (p-reduce
+  (fn [z n pn]
+    (when (zero? (mod pn 1000000))
+      (reduced n)))))
