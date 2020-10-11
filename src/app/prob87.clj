42c20edf9e338d5444f0f9fdd1bafa54eca83b9c
tarasov95 <lysy95@gmail.com>
2020-10-11 17:40:48 +0800

    prob87/distinct

---
+++ src/app/prob87.clj
@@ -0,0 +1,29 @@
+(ns app.prob87
+  (:require [clojure.test :as t]
+            [lib.seq :as sq]
+            [lib.prime :as prime]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.test.alpha :as stest]
+            [lib.numb :as numb]))
+
+;; https://projecteuler.net/problem=87
+
+(defn eligible-only [rg U]
+  (->> rg
+       (filter #(< % (- U 4 8)))))
+
+(defn list-powers [U]
+  (let [p1 (prime/primes-below (numb/lsqrt (- U 4 8)))
+        p2 (eligible-only (map * p1 p1) U)
+        p3 (eligible-only (map * p2 p1) U)
+        p4 (eligible-only (map * p3 p1) U)]
+    (->> (for [x p2  y p3 z p4]
+           (+ x y z))
+         (filter #(< % U)))))
+
+(defn solve []
+  (->> (list-powers 50000000)
+       (distinct)
+       (count)))
+
+(solve)
