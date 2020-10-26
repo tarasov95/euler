8128adc83da7ac17d09f7c9a5780a156f8b42d2f
tarasov95 <lysy95@gmail.com>
2020-10-26 21:57:00 +0800

    prob89

--- src/app/prob89.clj
+++ src/app/prob89.clj
@@ -45,8 +45,84 @@
 (t/deftest to10-test
   (t/is (= 4672 (to10 "MMMMDCLXXII"))))
 
-(let [data (slurp "resources/p089_roman.txt")
-      r    (st/split data #"\n")]
-  (->> r
-       (map to10)
-       (reduce max)))
+(def rdenom
+  (->> denom
+      (map identity)
+      (sort-by (comp - second))
+      (into [])))
+
+(def rdig
+  (->> denom
+       (map reverse)
+       (map (partial into []))
+       (into {})))
+
+(defn toR-denoms
+  ([n] (toR-denoms {} (map second rdenom) n))
+  ([z rd n]
+   (if (or (< n 1)
+           (empty? rd))
+     z
+     (let [d (first rd)
+           q (quot n d)]
+       (cond
+         (= 0 q) (recur z (rest rd) n)
+         :else (recur (assoc z d q) (rest rd) (mod n d)))))))
+
+(t/deftest toR-denoms-test
+  (t/is (= {1000 7, 500 1, 100 1, 10 4, 5 1, 1 3}
+           (toR-denoms 7648))))
+
+;; Only one I, X, and C can be used as the leading numeral in part of a subtractive pair.
+;; I can only be placed before V and X.
+;; X can only be placed before L and C.
+;; C can only be placed before D and M.
+(defmacro recur1 [md z R D]
+  `(recur (into ~z (repeat (~md ~D) ~R)) (dissoc ~md ~D)))
+
+(defmacro recur2 [md z R DD]
+  `(recur (conj ~z ~R) (apply dissoc ~md ~DD)))
+
+(defn toR-from-denoms [z md]
+  (if (empty? md)
+    z
+    (cond
+      (md 1000) (recur1 md z "M" 1000)
+      (and (= 1 (md 500))
+           (= 4 (md 100))) (recur2 md z "CM" [500 100])
+      (md 500) (recur1 md z "D" 500)
+      (= 4 (md 100)) (recur2 md z "CD" [100])
+      (md 100) (recur1 md z "C" 100)
+      (and (= 1 (md 50))
+           (= 4 (md 10))) (recur2 md z "XC" [50 10])
+      (md 50) (recur1 md z "L" 50)
+      (= 4 (md 10)) (recur2 md z "XL" [10])
+      (md 10) (recur1 md z "X" 10)
+      (and (= 1 (md 5))
+           (= 4 (md 1))) (recur2 md z "IX" [5 1])
+      (md 5) (recur1 md z "V" 5)
+      (= 4 (md 1)) (recur2 md z "IV" [1])
+      (md 1) (recur1 md z "I" 1)
+      :else [md z])))
+
+(defn toR [n]
+  (reduce
+   str
+   (toR-from-denoms [] (toR-denoms n))))
+
+(t/deftest toR-test
+  (t/is (= "VII" (toR 7)))
+  (t/is (= "II" (toR 2)))
+  (t/is (= "IV" (toR 4)))
+  (t/is (= "IX" (toR 9)))
+  (t/is (= "XIX" (toR 19))))
+
+(defn solve []
+  (let [data (slurp "resources/p089_roman.txt")
+        r    (st/split data #"\n")]
+    (->> r
+         (map #(- (count %) (count (-> % to10 toR))))
+         (reduce +))))
+
+(t/deftest solve-test
+  (t/is (= 743 (solve))))
